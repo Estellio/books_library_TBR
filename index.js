8bd392f5779e1e6ffba435e7616181c2c11a190f
Esellio <pirko.vichmann@voco.ee>
2023-03-22 15:45:59 +0200

    As a user I want to crate an account. [delivers #184447170]

--- index.js
+++ index.js
@@ -3,119 +3,62 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const yamlJs = require('yamljs');
 const swaggerDocument = yamlJs.load('./swagger.yaml');
-const {v4: uuidv4} = require('uuid');
+
+// Import bcrypt for password encryption
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // Number of salt rounds to use for encryption
 
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
-let sessions = [];
-let users = [
-    {id: 1, email: 'admin@admin.com', password: 'admin'}
-];
 // Serve static files
 app.use(express.static('public'));
 
-// Use the Swagger UI
+// Use the Swagger UI to display API documentation
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Middleware to parse JSON
 app.use(express.json());
 
-// General error handler
+
+// Create a user array to store users
+const users = [];
+
+// General error handler middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     const status = err.statusCode || 500;
     res.status(status).send(err.message);
-})
+});
 
-// Routes
-app.post('/sessions', (req, res) => {
-    // Validate email and password
-    if (!req.body.email || !req.body.password) {
-        return res.status(400).send('Email and password are required')
-    }
-    // Get user
-    const user = users.find(user => user.email === req.body.email)
-    if (!user) {
-        return res.status(400).send('User not found')
-    }
-    // Check password
-    if (user.password !== req.body.password) {
-        return res.status(400).send('Invalid password')
+// Endpoint to create a new user with email and password
+app.post('/users', (req, res) => {
+    const {email, password} = req.body;
+    const user = {email, password};
+
+    // validate the email and password
+    if (!email || !password) {
+        return res.status(400).send('Email and password are required');
+    } else if (users.find(u => u.email === email)) {
+        return res.status(409).send('Email already exists');
     }
 
-    // If valid, create a session
-    const session = {
-        id: uuidv4(),
-        userId: user.id,
-        createdAt: new Date()
-    }
+    // Hash the password with bcrypt before storing in the database
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error creating user');
+        } else {
+            user.password = hash;
+            users.push(user);
+            return res.status(201).send('User created');
+        }
+    });
+});
 
-    // Add session to sessions array
-    sessions.push(session)
-
-    // Return a new session
-    res.status(201).send({sessionId: session.id})
-})
-
-function authenticateRequest(req, res, next) {
-
-    // Check for authorization header
-    const authHeader = req.headers.authorization
-    if (!authHeader) {
-        return res.status(401).send('Authorization header is required')
-    }
-
-    // Check Authorization header format
-    const authHeaderParts = authHeader.split(' ')
-    if (authHeaderParts.length !== 2 || authHeaderParts[0] !== 'Bearer') {
-        return res.status(401).send('Authorization header format must be "Bearer {token}"')
-    }
-
-    // Get session ID from header
-    const sessionId = authHeaderParts[1]
-
-    // Validate session ID
-    if (!sessionId) {
-        return res.status(401).send('Session ID is required')
-    }
-
-    // Get session
-    const session = sessions.find(session => session.id === sessionId)
-
-    // If session not found, return 401
-    if (!session) {
-        return res.status(401).send('Invalid session')
-    }
-
-    // Get user
-    const user = users.find(user => user.id === session.userId)
-
-    // Validate user
-    if (!user) {
-        return res.status(401).send('User not found')
-    }
-
-    // Add session to request
-    req.session = session
-
-    // Add user to request
-    req.user = user
-
-    // Continue processing the request
-    next()
-}
-
-app.delete('/sessions', authenticateRequest, (req, res) => {
-
-    // Remove session from sessions array
-    sessions = sessions.filter(session => session.id !== req.session.id)
-
-    // Return a 204 with no content if the session was deleted
-    res.status(204).send()
-})
-
+// Start the server and connect to the database
 app.listen(port, () => {
-    console.log(`App running. Docs at http://localhost:${port}/docs`);
-})
+
+console.log(`App is running at http://localhost:${port} and docs are at http://localhost:${port}/docs`);
+});
